e31f8f83aa66cc49c0d324fbff2e630a297f7cb4
rhowell476 <rhowell91@live.com>
2022-10-31 18:30:01 -0500

    RacersMap node_swap() .extract(k1) .insert(move(node1)

---
+++ CPP_20_STL-Cookbook/RacersMap.cpp
@@ -0,0 +1,40 @@
+#include <map>
+#include <string>
+#include <iostream>
+#include <map>
+#include <format>
+
+using namespace std;
+using Racermap = map<unsigned int, string>;
+
+void printm(const Racermap &m){
+    cout << "Rank: \n";
+    for (const auto& [rank, racer] : m) {
+        cout << format("{}:{}\n", rank, racer);
+    }
+}
+
+template<typename M, typename K>
+bool node_swap(M& m, K k1, K k2) {
+    auto node1{ m.extract(k1) };
+    auto node2{ m.extract(k2) };
+    if (node1.empty() || node2.empty()) {
+        return false;
+    }
+    swap(node1.key(), node2.key());
+    m.insert(move(node1));
+    m.insert(move(node2));
+    return true;
+
+}
+
+int main() {
+    Racermap racers {
+        {1, "Mario"},{2, "Luigi"},{3, "Bowser"},
+        {4, "Peach"},{5, "Donkey Kong Jr"}
+    };
+    printm(racers);
+    node_swap(racers, 3, 5);
+    printm(racers);
+
+}
